2d8a7dbec7066d0b06b53ed21abda44267a36a58
Daniel Jr <danjun48@gmail.com>
2019-04-17 16:04:39 -0300

    Change Password Feature

---
+++ DiaOito/Controllers/PerfilController.cs
@@ -0,0 +1,53 @@
+﻿using DiaOito.Utils;
+using DiaOito.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DiaOito.Controllers
+{
+    [Authorize]
+    public class PerfilController : Controller
+    {
+        MeuMedicoEntities db = new MeuMedicoEntities();
+
+        // GET: Perfil
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult AlterarSenha()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AlterarSenha(AlterarSenhaViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+            var identity = User.Identity as ClaimsIdentity;
+            var login = identity.Claims.FirstOrDefault(c => c.Type == "Login").Value;
+            var usuario = db.Usuarios.FirstOrDefault(u => u.Usuario == login);
+
+            if (HashSha.GeraHash(viewModel.SenhaAtual) != usuario.Senha)
+            {
+                ModelState.AddModelError("SenhaAtual", "Senha atual incorreta");
+                return View(viewModel);
+            }
+
+            usuario.Senha = HashSha.GeraHash(viewModel.NovaSenha);
+            db.Entry(usuario).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+
+            return RedirectToAction("index", "home");
+        }
+    }
+}
